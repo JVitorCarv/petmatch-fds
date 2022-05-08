4f2ee9c898c431139061054e768948b9c50fa0c6
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-05-08 05:14:14 -0300

    Format authentication pages

--- petmatch/settings.py
+++ petmatch/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
     #local apps
     "petmatchapp.apps.PetmatchappConfig",
     
@@ -141,3 +142,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
+ACCOUNT_SESSION_REMEMBER = True
+
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = "bootstrap4"
