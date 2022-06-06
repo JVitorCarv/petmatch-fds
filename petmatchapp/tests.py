b74344e4d4efeafcb65a67e9448fe9e5e8ae2266
Dcmaran <diogocmr2002@gmail.com>
2022-06-05 23:18:05 -0300

    repair tests

--- petmatchapp/tests.py
+++ petmatchapp/tests.py
@@ -124,4 +124,17 @@ class testLogin(LiveServerTestCase):
 
         assert 'PetMatch' in driver.title
 
+
+class TestSupport(LiveServerTestCase):
+    def testSupport(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        driver.get('https://pet-match-fds.herokuapp.com/settings')
+        assert 'Settings' in driver.title
+
         driver.close()
