b681e664095f70ec5ca3ac9e57d2daa0252cfd98
PatCatchpole <pecc@cesar.school>
2022-06-05 21:34:50 -0300

    Implementação de contactar o support

--- petmatchapp/tests.py
+++ petmatchapp/tests.py
@@ -126,4 +126,21 @@ class testLogin(LiveServerTestCase):
 
         driver.close()
 
+class TestSupport(LiveServerTestCase):
+    def testSupport(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--window-size=1420,1080')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        driver.get('http://127.0.0.1:8000/settings/')
+        button = driver.find_element_by_name("support") 
+        button.click()
+
+        assert 'Settings' in driver.title
+
+        driver.close()
+
 
